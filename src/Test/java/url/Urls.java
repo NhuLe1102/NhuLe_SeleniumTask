9e9f453f03ebd83d13af23f9205846c13804a5f6
nhule <lenhu1944@gmail.com>
2022-01-22 15:29:57 +0700

    Lession17

--- src/Test/java/url/Urls.java
+++ src/Test/java/url/Urls.java
@@ -3,4 +3,8 @@ package Test.java.url;
     public interface Urls {
         String BASE_URL = "https://the-internet.herokuapp.com";
         String DYNAMIC_CONTROL_PAGE = "/dynamic_controls";
+        String HOVERS = "/hovers";
+        String IFRAME = "/iframe";
+        String JAVASCRIPT_ALERTS = "/javascript_alerts";
+        String DROPDOWN = "/dropdown";
     }
